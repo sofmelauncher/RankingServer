6eba19aabf2ebcdc0bac726f013bea80f9b74861
すー <s1732087ap@s.chibakoudai.jp>
2018-09-04 01:36:37 +0900

    aaa

--- GetData.php
+++ GetData.php
@@ -1,1 +1,18 @@
 <?php
+$keyword='';
+$resultData='';
+
+//データ受け取り
+if(isset($_POST['word'])){
+    $keyword=$_POST['word'];	//キーワード
+}else{
+    $keyword = "not word";
+}
+
+
+
+$resultData = $keyword;
+
+header('Content-type: application/plain');
+// print json_encode($resultData);
+print $resultData;
