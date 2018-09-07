e25e8f9cf84921a0a0933692c4bfccd4d76f1e70
すー <aaaiiiuuu35@gmail.com>
2018-09-07 21:12:37 +0900

    データベース接続（未実装）

--- GetData.php
+++ GetData.php
@@ -13,12 +13,31 @@ if(isset($_POST["GameID"])){
 }else{
     $data = "not word";
 }
+// $dsn = "mysql:dbname=sofmegameranking;host=localhost;charset=utf8mb4";
+// $username = "RankinServer";
+// $password = "sofmesomfe";
+// $driver_options = [
+//     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+//     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+// ];
+// try {
+//     $pdo = new PDO($dsn, $username, $password, $driver_options);
+// } catch (PDOException $e) {
+    
+//         // エラーが発生した場合は「500 Internal Server Error」でテキストとして表示して終了する
+//         // - もし手抜きしたくない場合は普通にHTMLの表示を継続する
+//         // - ここではエラー内容を表示しているが， 実際の商用環境ではログファイルに記録して， Webブラウザには出さないほうが望ましい
+//         header('Content-Type: text/plain; charset=UTF-8', true, 500);
+//         exit($e->getMessage()); 
+    
+// }
 
+
+
+    
 // $resultData = $keyword;
-
 header('Content-type: application/plain');
 // print json_encode($resultData);
 // print $data;
 print json_encode( $data ) ;
-
 
