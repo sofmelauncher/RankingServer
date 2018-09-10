d2e9cfacd4e9d95215bc71bb77a9be343ae67e44
すー <aaaiiiuuu35@gmail.com>
2018-09-10 18:47:45 +0900

    IDを指定してデータ取得

--- GetData.php
+++ GetData.php
@@ -1,43 +1,41 @@
 <?php
-$keyword='';
-$resultData='';
-$data = array();
-//データ受け取り
+require('config.php');
+
+
 if(isset($_POST["GameID"])){
-    $data["GameID"] = $_POST["GameID"];
-    $data["Time"] = $_POST["Time"];
-    $data["DataID"] = $_POST["DataID"];
-    $data["DataName"] = $_POST["DataName"];
-    $data["Score"] = $_POST["Score"];
-    
+    $id = $_POST["GameID"];
 }else{
-    $data = "not word";
+    $id = 0;
 }
-// $dsn = "mysql:dbname=sofmegameranking;host=localhost;charset=utf8mb4";
-// $username = "RankinServer";
-// $password = "sofmesomfe";
-// $driver_options = [
-//     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-//     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-// ];
-// try {
-//     $pdo = new PDO($dsn, $username, $password, $driver_options);
-// } catch (PDOException $e) {
-    
-//         // エラーが発生した場合は「500 Internal Server Error」でテキストとして表示して終了する
-//         // - もし手抜きしたくない場合は普通にHTMLの表示を継続する
-//         // - ここではエラー内容を表示しているが， 実際の商用環境ではログファイルに記録して， Webブラウザには出さないほうが望ましい
-//         header('Content-Type: text/plain; charset=UTF-8', true, 500);
-//         exit($e->getMessage()); 
-    
-// }
+$sql1 = "SELECT * FROM `GameID";
+$sql2 = "`ORDER BY SaveTime DESC;";
+
+try {
+    $pdo = new PDO($dns, $user, $pw, array(PDO::ATTR_EMULATE_PREPARES => false));
+    $statement = $pdo->query($sql1.strval($id).$sql2);
+} catch (PDOException $e) {
+    exit('データベース接続失敗。'.$e->getMessage());
+}
+print("終了");
 
 
-
-    
-// $resultData = $keyword;
-header('Content-type: application/plain');
+// header('Content-type: application/plain');
 // print json_encode($resultData);
 // print $data;
-print json_encode( $data ) ;
+// print json_encode( $statement ) ;
 
+// $members = array();
+// foreach ($statement as $row) {
+//     $members[] = $row;
+// }
+// var_dump($members);
+$userData = array();
+while($row = $statement->fetch(PDO::FETCH_ASSOC)){
+    $userData[]=array(
+    "DataID" => $row["DataID"],
+    "SaveTime" => $row['SaveTime'],
+    "DataName" => $row["DataName"],
+    "ScoreValue" => $row["ScoreValue"]
+    );
+}
+print json_encode($userData) ;
